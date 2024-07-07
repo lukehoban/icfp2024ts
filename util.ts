57dbfc92dd82785a539c686d0999c21a7feb525b
Luke Hoban <lukehoban@gmail.com>
2024-07-06 21:43:01 -0700

    Cleanup utils

--- util.ts
+++ util.ts
@@ -1,18 +1,14 @@
 
-const order = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n";
+const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n";
 
-export const decodeString = (s: string): string => {
-    
-    return [...s].map(c => order[c.charCodeAt(0) - 33]).join('');
-};
+export const decodeString = (s: string): string => 
+    [...s].map(c => chars[c.charCodeAt(0) - 33]).join('');
 
 export const fromBase94 = (s: string): number =>
-    [...s].reduce((acc, c) => acc * 94 + c.charCodeAt(0) - 33, 0);
+    [...s].reduce((acc, c) => acc * 94 + chars.indexOf(c), 0);
 
 export const toBase94 = (num: number): string => {
     const div = Math.floor(num / 94);
     const rem = num % 94;
-    return (div ? toBase94(div) : "") + order[rem];
+    return (div ? toBase94(div) : "") + chars[rem];
 }
-
-console.log(toBase94(15818151));
